4720f9aea00cd705cea570b8d7c212ec7b4e79c8
Dregierm <77828677+Dregierm@users.noreply.github.com>
2023-03-06 16:49:09 +0100

    Weryfikacja zapisu #.Xk

--- lab2/main.py
+++ lab2/main.py
@@ -5,10 +5,22 @@ import sys
 def my_printf(format_string,param):
     i = 0
     format_str_len = len(format_string)
+    num_len = 0
     while i < format_str_len:    
         if i+1 < format_str_len and format_string[i] == '#' and format_string[i+1] == 'k':
             print(param.swapcase(), end="")
             i += 1  
+        elif i+3 < format_str_len and format_string[i] == '#' and format_string[i+1] == '.':
+            i += 2
+            while format_string[i].isdigit():
+                num_len += 1
+                i += 1
+            if num_len == 0:
+                print(format_string[i-2:i+1], end="")
+            elif format_string[i] != 'k':
+                print(format_string[i-num_len-2:i+1], end="")
+            else:
+                #TODO  
         else:
                 print(format_string[i], end="")
         i += 1    
