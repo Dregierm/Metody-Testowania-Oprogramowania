d357731c81e419a5e80c7958c26f004c63ed90ec
Dregierm <77828677+Dregierm@users.noreply.github.com>
2023-03-13 16:30:53 +0100

    Dodanie obsługi #Xk

--- lab3/main.py
+++ lab3/main.py
@@ -4,17 +4,21 @@ import sys
 import re
 
 def my_printf(format_string, param):
-    search = re.search("#(\.\d+)?k", format_string)
+    search = re.search("#(\d+)?(\.\d+)?k", format_string)
     if not search:
         return print(format_string)
 
     param_len = len(param)
     param_print_len = param_len
-    if search.group(1): #.X
-        param_print_len = min(int(search.group(1)[1:]), param_len)
+    print_spaces = 0
+    if search.group(2): #.X
+        param_print_len = min(int(search.group(2)[1:]), param_len)
+
+    if search.group(1):
+        print_spaces = max(int(search.group(1)) - param_print_len, 0)        
 
     to_replace = format_string[search.start() : search.end()]
-    print(format_string.replace(to_replace, param.swapcase()[:param_print_len]))
+    print(format_string.replace(to_replace, " "*print_spaces + param.swapcase()[:param_print_len]))
 
 data = sys.stdin.readlines()
 
