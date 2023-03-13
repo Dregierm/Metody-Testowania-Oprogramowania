7cf438e2fabd683577a9d1525d0a8cd97c2f719b
Dregierm <77828677+Dregierm@users.noreply.github.com>
2023-03-13 16:27:00 +0100

    Wersja z wykorzystaniem re

--- lab3/main.py
+++ lab3/main.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
-def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+def my_printf(format_string, param):
+    search = re.search("#(\.\d+)?k", format_string)
+    if not search:
+        return print(format_string)
 
-data=sys.stdin.readlines()
+    param_len = len(param)
+    param_print_len = param_len
+    if search.group(1): #.X
+        param_print_len = min(int(search.group(1)[1:]), param_len)
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+    to_replace = format_string[search.start() : search.end()]
+    print(format_string.replace(to_replace, param.swapcase()[:param_print_len]))
+
+data = sys.stdin.readlines()
+
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
