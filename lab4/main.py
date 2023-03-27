122b7c2c4cbc86197921026a1edbd29bb19905a6
Dregierm <77828677+Dregierm@users.noreply.github.com>
2023-03-27 16:31:19 +0200

    Dodanie obsługi #g

--- lab4/main.py
+++ lab4/main.py
@@ -4,21 +4,34 @@ import sys
 import re
 
 def my_printf(format_string, param):
-    search = re.search("#(\d+)?(\.\d+)?k", format_string)
-    if not search:
+    search_k = re.search("#(\d+)?(\.\d+)?k", format_string)
+    search_g = re.search("#g", format_string)
+
+    if not search_k and not search_g:
         return print(format_string)
 
-    param_len = len(param)
-    param_print_len = param_len
-    print_spaces = 0
-    if search.group(2): #.X
-        param_print_len = min(int(search.group(2)[1:]), param_len)
+    if search_k:
+        param_len = len(param)
+        param_print_len = param_len
+        print_spaces = 0
 
-    if search.group(1):
-        print_spaces = max(int(search.group(1)) - param_print_len, 0)        
+        if search_k.group(2): #.X
+            param_print_len = min(int(search_k.group(2)[1:]), param_len)
 
-    to_replace = format_string[search.start() : search.end()]
-    print(format_string.replace(to_replace, " "*print_spaces + param.swapcase()[:param_print_len]))
+        if search_k.group(1): #Xk
+            print_spaces = max(int(search_k.group(1)) - param_print_len, 0)        
+
+        to_replace = format_string[search_k.start() : search_k.end()]
+        print(format_string.replace(to_replace, " "*print_spaces + param.swapcase()[:param_print_len]))
+    elif search_g:
+        to_replace = format_string[search_g.start() : search_g.end()]
+
+        if str(param).isnumeric():
+            print(format_string.replace(to_replace, str(param)[::-1]))
+        else:
+            return print(format_string)  
+
+            
 
 data = sys.stdin.readlines()
 
