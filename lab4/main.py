24908cd6c437af693d0f3c5a57a3451f80e65ec2
Dregierm <77828677+Dregierm@users.noreply.github.com>
2023-03-27 16:23:10 +0200

    lab3 #k

--- lab4/main.py
+++ lab4/main.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
-def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+def my_printf(format_string, param):
+    search = re.search("#(\d+)?(\.\d+)?k", format_string)
+    if not search:
+        return print(format_string)
 
-data=sys.stdin.readlines()
+    param_len = len(param)
+    param_print_len = param_len
+    print_spaces = 0
+    if search.group(2): #.X
+        param_print_len = min(int(search.group(2)[1:]), param_len)
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+    if search.group(1):
+        print_spaces = max(int(search.group(1)) - param_print_len, 0)        
+
+    to_replace = format_string[search.start() : search.end()]
+    print(format_string.replace(to_replace, " "*print_spaces + param.swapcase()[:param_print_len]))
+
+data = sys.stdin.readlines()
+
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
