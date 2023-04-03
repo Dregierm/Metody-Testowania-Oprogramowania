f499203fff3a9e7afc7ee11465a4563a7f9b00d5
Dregierm <77828677+Dregierm@users.noreply.github.com>
2023-04-03 16:28:49 +0200

    #Xg

--- lab5/main.py
+++ lab5/main.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
-def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+def my_printf(format_string, param):
+    search_g = re.search("#(\d+)?g", format_string)
 
-data=sys.stdin.readlines()
+    if not search_g:
+        return print(format_string)
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+    to_replace = format_string[search_g.start() : search_g.end()]
+    param_len = len(str(param))
+
+    if str(param).isnumeric():
+            if search_g.group(1): #Xg
+                print_spaces = max(int(search_g.group(1)) - param_len, 0)
+                param = list(str(param))
+                for i in range(param_len):
+                    param[i] = str(int(param[i]) - 1) if param[i] != '0' else '9'
+                param = str(''.join(param))
+                return print(format_string.replace(to_replace,  " "*print_spaces + param))    
+            else: #g
+                print(format_string.replace(to_replace, str(int(str(param)[::-1]))))
+    else:
+        return print(format_string)  
+
+
+            
+data = sys.stdin.readlines()
+
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
