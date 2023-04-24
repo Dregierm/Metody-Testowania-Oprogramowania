ccb45ec9a4121b82d8c6b2ae08e3405dbfa0cdec
Dregierm <77828677+Dregierm@users.noreply.github.com>
2023-04-24 16:23:50 +0200

    Implementacja #j

--- lab7/main.py
+++ lab7/main.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
-def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+replacer = {
+    'a': 'g',
+    'b': 'h',
+    'c': 'i',
+    'd': 'j',
+    'e': 'k',
+    'f': 'l',
+}
 
-data=sys.stdin.readlines()
+def my_printf(format_string, param):
+    search = re.search("#j", format_string)
+    if not search:
+        return print(format_string)
+    
+    to_replace = format_string[search.start() : search.end()]
+    hex_param = list(str(hex(int(param)))[2:])
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+    for i in range(len(hex_param)):
+        if not hex_param[i].isnumeric():
+            hex_param[i] = replacer[hex_param[i]]
+    hex_param = str(''.join(hex_param))    
+
+    print(format_string.replace(to_replace, hex_param))
+
+data = sys.stdin.readlines()
+
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
